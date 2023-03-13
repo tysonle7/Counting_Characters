7f71b9858ac95899f5e8d6d81117028be5112b6c
tysonle7 <125998611+tysonle7@users.noreply.github.com>
2023-03-13 17:23:26 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;  
+public class Program {  
+    public static void Main() {  
+        string strFirst;  
+        char charCout;  
+        int Count = 0;  
+        Console.Write("Enter Your String:");  
+        strFirst = Console.ReadLine();  
+        Console.Write("Enter Count Character:");  
+        charCout = Convert.ToChar(Console.ReadLine());  
+        Console.Write("Your Character Count:");  
+        foreach(char chr in strFirst) {  
+            if (chr == charCout) {  
+                Count++;  
+            }  
+        }  
+        Console.Write(Count);  
+    }  
+}  
